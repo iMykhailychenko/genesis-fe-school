7cdb38e521869b22ad5db3583e6a4b8aa849d152
iMykhailychenko <igor.c.m@ukr.net>
2023-03-19 20:00:57 +0000

    Deploying to gh-pages from @ iMykhailychenko/genesis-fe-school@5acae30d4925ecd198e20cc2e98e4ff6c112603f 🚀

--- assets/chunk-B2MGPQRJ-d7ab808d.js
+++
@@ -1,1 +0,0 @@
-import{f as c,s as i,o as r,j as t,c as o,a as m}from"./index-b0768b19.js";var d=c(function(a,s){const e=i("Heading",a),{className:f,...n}=r(a);return t(o.h2,{ref:s,className:m("chakra-heading",a.className),...n,__css:e})});d.displayName="Heading";export{d as H};
