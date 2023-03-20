f293407469d36a95bcfffbcc2cafb5837eb3bf70
iMikhaylichenko <igor.c.m@ukr.net>
2023-03-20 08:56:38 +0100

    error page

--- src/components/auth-wrapper/auth-wrapper.tsx
+++ src/components/auth-wrapper/auth-wrapper.tsx
@@ -1,5 +1,8 @@
 import React, { ReactNode } from 'react';
 
+import { Button, Center, Image, Stack, Text } from '@chakra-ui/react';
+
+import { NetworkError } from '@app/components/network-error/network-error';
 import { PageLoader } from '@app/components/page-loader/page-loader';
 import { useAuthQuery } from '@app/queries/auth/auth.hooks';
 
@@ -14,7 +17,16 @@ export const AuthWrapper = ({ children }: Props): JSX.Element => {
     }
 
     if (isError) {
-        return <>Error</>;
+        return (
+            <Center h="100vh">
+                <Stack spacing={10} alignItems="center">
+                    <Image src="/genesis-fe-school/disconnected.png" alt="Network error" w="200px" />
+                    <Text w="250px" textAlign="center">
+                        Ooops. Something went wrong
+                    </Text>
+                </Stack>
+            </Center>
+        );
     }
 
     return <>{children}</>;
